09da31d8b31c5075d3a2816c4b55c806084f188e
Tom Victor <vjtomvictor@icloud.com>
2017-10-11 08:32:57 +0530

    succesfully compiled the classifier code

--- quickstart.py
+++ quickstart.py
@@ -26,6 +26,8 @@ def main():
     raw = raw.decode('utf-8')
     with open(IRIS_TRAINING, "w") as f:
       f.write(raw)
+  else:
+    print("iris_training.csv already exists")
 
   if not os.path.exists(IRIS_TEST):
     # raw = urllib.urlopen(IRIS_TEST_URL).read()
@@ -35,6 +37,8 @@ def main():
     raw = raw.decode('utf-8')
     with open(IRIS_TEST, "w") as f:
       f.write(raw)
+  else:
+    print("iris_test.scv already exists")
 
   # Load datasets.
   training_set = tf.contrib.learn.datasets.base.load_csv_with_header(
@@ -53,7 +57,7 @@ def main():
   classifier = tf.estimator.DNNClassifier(feature_columns=feature_columns,
                                           hidden_units=[10, 20, 10],
                                           n_classes=3,
-                                          model_dir="/tmp/iris_model")
+                                          model_dir="./tmp/iris_model")
   # Define the training inputs
   train_input_fn = tf.estimator.inputs.numpy_input_fn(
       x={"x": np.array(training_set.data)},
