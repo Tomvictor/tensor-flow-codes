cc98f359b5aa19ba5609addecccc4fbeb4faafa8
Tom Victor <vjtomvictor@icloud.com>
2017-10-12 09:55:52 +0530

    findin the loss function

---
+++ variables.py
@@ -0,0 +1,22 @@
+import tensorflow as tf 
+
+print("Starting...")
+sess = tf.Session()
+W = tf.Variable([.3], dtype=tf.float32)
+b = tf.Variable([-.3], dtype=tf.float32)
+x = tf.placeholder(tf.float32)
+linear_model = W * x + b
+# print(linear_model)
+#variables will never initialized until we run the call below
+init = tf.global_variables_initializer()
+sess.run(init)
+print(sess.run(linear_model,{x:[1,2,3,4]}))
+
+# loss function
+# Aloss function measures how far apart the current model is from the provided data
+# which is the sum of the deltas between the current model and the provided data
+y = tf.placeholder(tf.float32)
+squared_deltas = tf.square(linear_model-y)
+loss = tf.reduce_sum(squared_deltas)
+print(sess.run(loss,{x:[1,2,3,4],y:[0,-1,-2,-3]}))
+
